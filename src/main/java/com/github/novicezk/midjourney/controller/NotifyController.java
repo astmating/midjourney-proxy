79a1ead2db6d86ff667f720d7b909f5659f3e7d4
astmating <131445482+astmating@users.noreply.github.com>
2023-05-10 17:55:42 +0800

    NotifyController.java

--- src/main/java/com/github/novicezk/midjourney/controller/NotifyController.java
+++ src/main/java/com/github/novicezk/midjourney/controller/NotifyController.java
@@ -1,5 +1,30 @@
+package com.github.novicezk.midjourney.controller;
+
+import com.github.novicezk.midjourney.support.task.Task;
+import com.github.novicezk.midjourney.support.task.TaskHelper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 @RestController
 @RequestMapping("/notify")
 @RequiredArgsConstructor
-public class TaskController {
+public class NotifyController {
   private final NotifyService notifyService;
+	private final TaskHelper taskHelper;
+
+	@GetMapping("/list")
+	public List<Task> listTask() {
+		return this.taskHelper.listTask();
+	}
+
+	@GetMapping("/{id}/fetch")
+	public Task getTask(@PathVariable String id) {
+		return this.taskHelper.findById(id);
+	}
+
+}
